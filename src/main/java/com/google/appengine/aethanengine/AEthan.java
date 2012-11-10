8a4f8f1c2377d98dfdba89d2c58c88ff6562997c
iamkhova@gmail.com <>
2012-11-10 23:39:26 +0000

    Final Comimit for the night

---
+++ src/main/java/com/google/appengine/aethanengine/AEthan.java
@@ -0,0 +1,51 @@
+package com.google.appengine.aethanengine;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class AEthan
+{
+
+
+
+private static final Logger LOG = Logger.getLogger(MainServlet.class.getName());
+
+
+
+	CTeam homeTeam;
+	CTeam awayTeam;
+	GameStats stats;
+	
+	public AEthan(){
+	// Initialize Class
+		
+	homeTeam = new CTeam();
+	awayTeam = new CTeam();
+	stats = new GameStats();
+		
+	}
+	
+	public void createTest(){
+	// Create Sample Data Set To Run Test
+		
+		// Creating Two Teams for Test
+		homeTeam.createTeam("Raleigh", "Storm");
+    LOG.info("Home Team Created");
+
+		awayTeam.createTeam("Cary", "WildCats");
+		LOG.info("Away Team Created");
+
+	}
+	
+	public GameStats simGame(){
+		 
+		LOG.info("Simming Game... ");
+  
+		stats.homeTeamFinalScore = 5;
+		stats.awayTeamFinalScore = 6;
+		
+		return this.stats;
+	}
+		
+}
