59ed8ebd42629369dbec17f582299146d231feea
iamkhova@gmail.com <>
2013-06-25 01:20:29 +0000

    Added coded to handle Ball.

---
+++ src/main/java/com/google/appengine/aethanengine/CBall.java
@@ -0,0 +1,28 @@
+package com.google.appengine.aethanengine;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.UUID;
+
+public class CBall
+{
+   // Implement Logging
+   private static final Logger LOG = Logger.getLogger(MainServlet.class.getName());
+   
+   UUID ballID;
+   int speed;
+   int direction;
+   int type;
+   int state;
+   
+   public CBall()
+   {
+      // Initalize Class
+      LOG.info("Initalizing CBall()");
+      
+      ballID = UUID.randomUUID();
+      speed = 0;
+      direction = 0;
+      type = 0;
+      state = 0;     
+   }
+}
